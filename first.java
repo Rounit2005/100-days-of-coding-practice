cc1683e6d9845b88c536cc71e50d6bacf924db9b
rounit568 <rounitkashyap568@gmail.com>
2025-03-07 15:43:52 +0530

    solve one question

--- first.java
+++ first.java
@@ -1,5 +1,16 @@
+// class first{
+//     public static void main(String[] args){
+//         System.out.println("Hello , Rounit !");
+//     }
+// }
+
+
 class first{
     public static void main(String[] args){
-        System.out.println("Hello , Rounit !");
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println("The sum of a and b is : " + c);
     }
 }
+
