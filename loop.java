fd25e73abb3101ba9f528797cac0487cb16c24ca
rounit568 <rounitkashyap568@gmail.com>
2025-03-15 21:53:58 +0530

    DAY : 3

---
+++ loop.java
@@ -0,0 +1,5 @@
+class loop{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner
+    }
+}
--- string.java
+++ string.java
@@ -1,7 +1,61 @@
+
+// string introduction 
+
+// class string{
+//     public static void main(String[] args) {
+//         String s = "Hey Bro i am practicing 100 days of code ";
+//         System.out.println(s);
+
+//     }
+// }
+
+
+
+// import java.util.*;
+// class string{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter the name : ");
+//         String a =sc.next();
+//         System.out.println("Your name is " + a);
+//     }
+// }
+
+
+// import java.util.*;
+// class string {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner (System.in);
+//         System.out.print("Introduce your self : ");
+//         String a = sc.nextLine();
+//         String b = sc.nextLine();
+//         System.out.println("Hey its me " + a + " pursuing " + b);
+
+//     }
+// }
+
+// import java. util.*;
+// class string {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner (System.in);
+//         String ch = sc.nextLine();
+//         char p = ch.charAt(2);
+//         System.out.println(p);
+
+
+//     }
+
 class string{
     public static void main(String[] args) {
-        String s = "Hey Bro i am practicing 100 days of code ";
-        System.out.println(s);
-
+        String a = "Rounit kashyap";
+        String b = " And his life is best";
+        System.out.println(a.startsWith("Rou"));
+        System.out.println(a.startsWith("yap"));
+        System.out.println(a.endsWith("Rou"));
+        System.out.println(a.endsWith("yap"));
+        System.out.println(a.contains("Rounit"));
+        System.out.println(a.contains("jam"));
+        System.out.println(a.concat(b));
+        
     }
 }
