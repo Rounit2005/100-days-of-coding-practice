cf90f7f47e201b42d8f951c6825c35e79fe78952
rounit568 <rounitkashyap568@gmail.com>
2025-03-17 14:20:16 +0530

    DAY : 4

---
+++ Array.java
@@ -0,0 +1,34 @@
+// class Array{
+//     public static void main(String[] args) {
+//         int[] arr = {2,4,5,6,7,8};
+//         System.out.print(arr[0] + " ");
+//         System.out.print(arr[1] + " ");
+//         System.out.print(arr[2] + " ");
+//         System.out.print(arr[3] + " ");
+//         System.out.print(arr[4] + " ");
+//         // System.out.print(arr[5]);
+//         arr[5] = 90;
+//         System.out.println(arr[5]);
+//     }
+// }
+
+// class Array {
+//     public static void main(String[] args) {
+//         int[] arr = new int[5];
+//         arr[0] = 5;
+//         arr[1] = 10;
+//         arr[2] = 20;
+//         arr[3] = 30;
+//         arr[4] = 40;
+//         System.out.println(arr[3]);
+//     }
+// }
+
+class Array{
+    public static void main(String[] args) {
+        int [] x = {2, 4, 6, 7, 9};
+        for(int i =0; i<=4; i++){
+            System.out.println(x[i]);
+        }
+    }
+}
